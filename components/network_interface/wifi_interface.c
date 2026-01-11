f042cd4d05dc4f894f5324dc88bac27d6708dff0
craigmillard86 <craigmillard@gmail.com>
2026-01-11 17:03:30 +0000

    Added back removed wifi power lines in wifi_interface.c

--- components/network_interface/wifi_interface.c
+++ components/network_interface/wifi_interface.c
@@ -195,6 +195,9 @@ void wifi_start(void) {
   esp_wifi_netif = esp_netif_create_wifi(WIFI_IF_STA, &esp_netif_config);
   esp_wifi_set_default_wifi_sta_handlers();
 
+  // esp_wifi_set_ps(WIFI_PS_MIN_MODEM);
+  //   esp_wifi_set_ps(WIFI_PS_NONE);
+
 #if ENABLE_WIFI_PROVISIONING
   /* Start Wi-Fi station */
   ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
